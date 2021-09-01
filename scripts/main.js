e8ef783a444292c6717734871bbd6ff9cbc390eb
Mikael <dev.kov.mtl@gmail.com>
2021-09-01 15:10:26 -0400

    Etch a Sketch - Added grid

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,48 @@
 let gridContainer = document.querySelector('#grid-container')
 
-let num = 4
+let num = 100
 
-for (let row = 0; row < num; row++) {
-  let rowContainer = document.createElement('div')
-  rowContainer.setAttribute('class', 'grid-row')
-  rowContainer.style.height = `calc(10% * ${num})`
+function createGrid(num) {
+  for (let row = 0; row < num; row++) {
+    let rowContainer = document.createElement('div')
+    rowContainer.setAttribute('class', 'grid-row')
+    rowContainer.style.height = `calc(10% * ${num})`
 
-  for (let col = 0; col < num; col++) {
-    // console.log(gridContainer)
-    let div = document.createElement('div')
-    div.classList.add('grid-cell')
-    div.style.width = `calc(100% / ${num})`
-    div.setAttribute('data-cell', `${row}x${col}`)
-    // console.log(`GRID CELL : ${row} ${col}`)
-    rowContainer.appendChild(div)
+    for (let col = 0; col < num; col++) {
+      // console.log(gridContainer)
+      let div = document.createElement('div')
+      div.classList.add('grid-cell')
+      div.style.width = `calc(100% / ${num})`
+      div.setAttribute('data-cell', `${row}x${col}`)
+      // console.log(`GRID CELL : ${row} ${col}`)
+      rowContainer.appendChild(div)
+    }
+
+    gridContainer.appendChild(rowContainer)
   }
 
-  gridContainer.appendChild(rowContainer)
+  const gridCells = document.querySelectorAll('div.grid-cell')
+  gridCells.forEach((gridCell) => {
+    gridCell.addEventListener('mouseover', onMouseOverGrid)
+  })
 }
+
+function onMouseOverGrid(e) {
+  const dataGridLocation = e.target.getAttribute('data-cell')
+  const cell = document.querySelector(
+    `div.grid-cell[data-cell="${dataGridLocation}"]`
+  )
+  cell.classList.add('color-cell')
+}
+
+const clearBtn = document.querySelector('button#clear')
+clearBtn.addEventListener('click', onClearBtn)
+
+function onClearBtn(e) {
+  console.log('clear')
+  let cellNbr = parseInt(prompt('number of cell'))
+  gridContainer.removeChild()
+  createGrid(cellNbr)
+}
+
+createGrid(48)
