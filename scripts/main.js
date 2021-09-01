db637b0ac161ac34e05f385b55ce9f644e472332
Mikael <dev.kov.mtl@gmail.com>
2021-09-01 09:41:15 -0400

    Etch a Sketch - create grid container created with JavaScript

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,20 @@
 let gridContainer = document.querySelector('#grid-container')
-console.log(gridContainer)
+
+let num = 4
+
+for (let row = 0; row < num; row++) {
+  let rowContainer = document.createElement('div')
+  rowContainer.setAttribute('class', 'grid-row')
+  rowContainer.style.height = `calc(10% * ${num})`
+
+  for (let col = 0; col < num; col++) {
+    // console.log(gridContainer)
+    let div = document.createElement('div')
+    div.classList.add('grid-cell')
+    div.style.width = `calc(100% / ${num})`
+    console.log(`GRID CELL : ${row} ${col}`)
+    rowContainer.appendChild(div)
+  }
+
+  gridContainer.appendChild(rowContainer)
+}
