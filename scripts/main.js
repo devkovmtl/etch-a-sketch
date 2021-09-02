edd631bfdf6cc5e8d1550814f0313c0d8eb17520
Mikael <dev.kov.mtl@gmail.com>
2021-09-02 13:32:33 -0400

    Paint the grid in black

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,47 @@
 let gridSize = 50
+// cell color
+let cellColorR = 0
+let cellColorG = 0
+let cellColorB = 0
+let cellColorOpacity = 1
+
 // INPUT RANGE SLIDER THE SLIDER AND VALUE
 const rangeGridSize = document.querySelector('#range-grid-size')
 const rangeGridValue = document.querySelector('.range-slider__value')
-// GRab the grid container
+// Grab the grid container
 let gridContainer = document.querySelector('.grid-container')
+// Grab the reset button and reset the btn
+const clearBtn = document.querySelector('button.btn-reset')
+clearBtn.addEventListener('click', (e) => createGrid(gridSize))
 
+// change the size of grid
 rangeGridSize.onchange = function (e) {
   gridSize = parseInt(e.target.value)
   rangeGridValue.textContent = gridSize
   createGrid(gridSize)
 }
 
-function createGrid(cellNbr) {
+function onMouseOverCell(e) {
+  const dataGridLocation = e.target.getAttribute('data-cell')
+  const cell = document.querySelector(
+    `div.grid-container__cell[data-cell="${dataGridLocation}"]`
+  )
+  cell.style.backgroundColor = `rgba(${cellColorR}, ${cellColorG}, ${cellColorB}, ${cellColorOpacity})`
+}
+
+// clear the grid
+function clearGrid() {
+  // CLEAR the grid
   while (gridContainer.firstChild) {
     gridContainer.removeChild(gridContainer.firstChild)
   }
+}
+
+// create the grid
+function createGrid(cellNbr) {
+  // first clear the grid
+  clearGrid()
+
   for (let row = 0; row < cellNbr; row++) {
     let rowContainer = document.createElement('div')
     rowContainer.setAttribute('class', 'grid-container__row')
@@ -32,32 +59,12 @@ function createGrid(cellNbr) {
 
     gridContainer.appendChild(rowContainer)
   }
+
+  // add mouse over listener to each cell
+  const gridCells = document.querySelectorAll('.grid-container__cell')
+  gridCells.forEach((gridCell) => {
+    gridCell.addEventListener('mouseover', onMouseOverCell)
+  })
 }
 
 createGrid(gridSize)
-
-//   const gridCells = document.querySelectorAll('div.grid-cell')
-//   gridCells.forEach((gridCell) => {
-//     gridCell.addEventListener('mouseover', onMouseOverGrid)
-//   })
-// }
-
-// function onMouseOverGrid(e) {
-//   const dataGridLocation = e.target.getAttribute('data-cell')
-//   const cell = document.querySelector(
-//     `div.grid-cell[data-cell="${dataGridLocation}"]`
-//   )
-//   cell.classList.add('color-cell')
-// }
-
-// const clearBtn = document.querySelector('button#clear')
-// clearBtn.addEventListener('click', onClearBtn)
-
-// function onClearBtn(e) {
-//   console.log('clear')
-//   let cellNbr = parseInt(prompt('number of cell'))
-//   while (gridContainer.firstChild) {
-//     gridContainer.removeChild(gridContainer.firstChild)
-//   }
-//   createGrid(cellNbr)
-// }
